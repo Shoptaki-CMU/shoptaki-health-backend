7a7263b5d78e713aaa982b2dbeb659707e43ad3e
yyxqqq777 <yunxuan517@gmail.com>
2023-04-06 23:34:32 -0400

    feat: update drug models and DrugSerializer

--- drugs/models.py
+++ drugs/models.py
@@ -5,4 +5,9 @@ class Drug(Collection):
     __collection__ = 'drug-label'
 
     description = List(String(required=True, allow_none=False))
+    adverse_reactions_table = List(String(required=True, allow_none=False))
+    dosage_and_administration_table = List(String(required=True, allow_none=False))
+    drug_interactions_table = List(String(required=True, allow_none=False))
+    warnings_and_cautions_table = List(String(required=True, allow_none=False))
+    precautions_table = List(String(required=True, allow_none=False))
 
--- drugs/serializers.py
+++ drugs/serializers.py
@@ -4,6 +4,9 @@ from .models import Drug
 class DrugSerializer(serializers.Serializer):
     _key = serializers.CharField()
     description = serializers.ListField(child=serializers.CharField())
+    adverse_reactions_table = serializers.ListField(child=serializers.CharField())
+    dosage_and_administration_table = serializers.ListField(child=serializers.CharField())
+    drug_interactions_table = serializers.ListField(child=serializers.CharField())
+    warnings_and_cautions_table = serializers.ListField(child=serializers.CharField())
+    precautions_table = serializers.ListField(child=serializers.CharField())
 
-    # def create(self, validated_data):
-    #     return Drug(**validated_data)
